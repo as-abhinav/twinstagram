92bcac673b2d9580d286ae91b7cde11f14d899a2
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2013-09-07 22:11:07 +0530

    [Sumit/Shridhar] - Editing routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,8 @@
 exports.routes = function (map) {
     map.root('app#index');
-    //map.get('*', 'app#index', {as: 'homePage'});
+    
     map.resources('posts')
+    map.get('homePage', 'app#index', {as: 'homePage'});
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
